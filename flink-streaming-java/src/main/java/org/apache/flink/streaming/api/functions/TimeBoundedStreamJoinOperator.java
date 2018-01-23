d672f052386ff7636de5591b8d8c4d9b45450440
Florian Schmidt <florian.schmidt.1994@icloud.com>
2018-01-23 17:23:54 +0100

    License headers and import order

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/TimeBoundedStreamJoinOperator.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/TimeBoundedStreamJoinOperator.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.streaming.api.functions;
 
 import org.apache.flink.annotation.VisibleForTesting;
@@ -117,7 +135,7 @@ public class TimeBoundedStreamJoinOperator<T1, T2>
 	}
 
 	/**
-	 * Process a {@link StreamRecord<T>} from the left stream. Whenever an {@link StreamRecord<T>}
+	 * Process a {@link StreamRecord<T1>} from the left stream. Whenever an {@link StreamRecord<T1>}
 	 * arrives at the left stream, it will get added to the left buffer. Possible join candidates
 	 * for that element will be looked up from the right buffer and if the pair lies within the
 	 * user defined boundaries, it gets collected.
