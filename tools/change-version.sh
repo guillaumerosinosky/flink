6c52da40224b7505bdd279d78497f6b926785994
Guillaume Rosinosky <guillaumerosinosky@yahoo.fr>
2020-12-09 14:01:50 +0000

    switch ar version to 1.7-AR-SNAPSHOT, caution dependency on 1.7-SNAPSHOT (flink-test-utils)

--- tools/change-version.sh
+++ tools/change-version.sh
@@ -17,8 +17,8 @@
 # limitations under the License.
 ################################################################################
 
-OLD="1.3-SNAPSHOT"
-NEW="1.4-SNAPSHOT"
+OLD="1.7-SNAPSHOT"
+NEW="1.7-AR-SNAPSHOT"
 
 
 HERE=` basename "$PWD"`
