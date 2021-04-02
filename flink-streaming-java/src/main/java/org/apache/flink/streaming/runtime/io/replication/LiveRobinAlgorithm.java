d771a861d94e215f030f960ca03f32f682860d0d
Guillaume Rosinosky <guillaumerosinosky@yahoo.fr>
2021-04-02 11:25:53 +0000

    liverobin: add queue processing

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/io/replication/LiveRobinAlgorithm.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/io/replication/LiveRobinAlgorithm.java
@@ -1,17 +1,11 @@
 package org.apache.flink.streaming.runtime.io.replication;
 
-import org.apache.flink.streaming.runtime.streamrecord.EndOfEpochMarker;
 import org.apache.flink.streaming.runtime.streamrecord.StreamElement;
-import org.apache.flink.util.Preconditions;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class LiveRobinAlgorithm extends Chainable {
 	private static final Logger LOG = LoggerFactory.getLogger(LiveRobinAlgorithm.class);
@@ -19,8 +13,10 @@ public class LiveRobinAlgorithm extends Chainable {
     private long highestHeartbeat = 0;
     private long currentHeartbeat = 0;
     private long[] heartbeatAtChannel;
-    private long nextQueueToPoll;
+    private int nextQueueToPoll;
     private int numProducers;
+
+    private Map<Integer, LinkedBlockingQueue<StreamElement>> messages;
 
     public LiveRobinAlgorithm(int numProducers) {
         this.heartbeatAtChannel = new long[numProducers];
@@ -29,35 +25,55 @@ public class LiveRobinAlgorithm extends Chainable {
         }
         this.nextQueueToPoll = 0;
         this.numProducers = numProducers;
-	}
-
+		this.messages = new HashMap<>();
+        for (int i = 0; i < numProducers; i++) {
+            this.messages.put(i, new LinkedBlockingQueue<StreamElement>());
+        }
+    }
 
 	@Override
 	public void accept(StreamElement element, int channel) throws Exception {
-        if (element.isEndOfEpochMarker()) { // read HB
-            if (element.getEpoch() > highestHeartbeat) {  // first?
-                if (this.hasNext()) { // propagate
-                    this.getNext().accept(element, channel);
-                    highestHeartbeat = element.getEpoch();
-                }
-            }
-            heartbeatAtChannel[channel] = element.getEpoch();
-            long lastProducers = this.currentHeartbeat;
-            for (int i = 0; i < numProducers; i++) {
-                if (heartbeatAtChannel[i] == this.currentHeartbeat) {
-                    this.nextQueueToPoll = i;
-                    lastProducers--;
-                }
-            }
-            if (lastProducers <= 0) {
-                this.currentHeartbeat++;
-            }
+        messages.get(channel).add(element);
+        processingLoop();
+    }
 
-        } else {
-            if (channel == this.nextQueueToPoll) {
-                this.getNext().accept(element, channel);
+    public void processingLoop() throws Exception {
+        boolean processing = true;
+        while (processing) {
+            StreamElement nextElement = messages.get(this.nextQueueToPoll).poll();
+            if (nextElement == null) {
+                LOG.debug("No more elements to poll on channel {}", this.nextQueueToPoll);
+                return;
             }
-        }
+            
+            if (nextElement.isEndOfEpochMarker()) { // read HB
+                if (nextElement.getEpoch() > highestHeartbeat) {  // first?
+                    if (this.hasNext()) { // propagate
+                        LOG.debug("Heartbeat propagation (epoch {})", this.currentHeartbeat);
+                        this.getNext().accept(nextElement, this.nextQueueToPoll);
+                        highestHeartbeat = nextElement.getEpoch();
+                    }
+                }
+                heartbeatAtChannel[this.nextQueueToPoll] = nextElement.getEpoch();
+                long lastProducers = this.currentHeartbeat;
+                for (int i = 0; i < numProducers; i++) {
+                    if (heartbeatAtChannel[i] == this.currentHeartbeat) {
+                        this.nextQueueToPoll = i;
+                        lastProducers--;
+                    }
+                }
+                if (lastProducers <= 0) {
+                    LOG.debug("All heartbeats {} processed, increase heartbeat to {}", this.currentHeartbeat, this.currentHeartbeat + 1);
+                    this.currentHeartbeat++;
+                }
+
+            } else {
+                if (this.hasNext()) {
+                    LOG.debug("Processing of element (epoch {})", this.currentHeartbeat);
+                    this.getNext().accept(nextElement, this.nextQueueToPoll);
+                }
+            }
+        }            
     }
 
 }
